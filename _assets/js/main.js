fd51f3af06b6a2c122e9141c51abefc0a66f3fde
l3njo <plenjo15@gmail.com>
2019-06-18 02:24:25 +0300

    Consolidate js

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,3 +1,31 @@
+"use strict";
 $(document).ready( function () {
     $('.datatable').DataTable();
+  
+    var btnQr = document.getElementById("btnQr");
+    var qrBack = document.getElementById("qrBack");
+    var qrCard = document.getElementById("qrCard");
+    var vcrCard = document.getElementById("voucherCard");
+    btnQr.onclick = function() {
+        vcrCard.style.display = "none";
+        qrCard.style.display = "block";
+    };
+    qrBack.onclick = function() {
+        qrCard.style.display = "none";
+        vcrCard.style.display = "block";
+    };
+
+    var visibleCard = document.getElementById("visibleNotes");
+    var hiddenCard = document.getElementById("hiddenNotes");
+    var btnShow = document.getElementById("btnShow");
+    var btnHide = document.getElementById("btnHide");
+    
+    btnShow.onclick = function() {
+        visibleCard.style.display = "none";
+        hiddenCard.style.display = "block";  
+    };
+    btnHide.onclick = function() {
+        visibleCard.style.display = "block";
+        hiddenCard.style.display = "none";  
+    };
 } );
--- _includes/footer.php
+++ _includes/footer.php
@@ -1,34 +1,5 @@
 <div class="fixed-bottom text-center bg-dark text-light">&copy <?=date("Y");?> !boop</div>
 <?php include '_includes/_modal.php'; ?>
-<script type="text/javascript">
-    var btnQr = document.getElementById("btnQr");
-    var qrBack = document.getElementById("qrBack");
-    var qrCard = document.getElementById("qrCard");
-    var vcrCard = document.getElementById("voucherCard");
-    btnQr.onclick = function() {
-        vcrCard.style.display = "none";
-        qrCard.style.display = "block";
-    };
-    qrBack.onclick = function() {
-        qrCard.style.display = "none";
-        vcrCard.style.display = "block";
-    };
-</script>
-<script type="text/javascript">
-        var visibleCard = document.getElementById("visibleNotes");
-        var hiddenCard = document.getElementById("hiddenNotes");
-        var btnShow = document.getElementById("btnShow");
-        var btnHide = document.getElementById("btnHide");
-        
-        btnShow.onclick = function() {
-            visibleCard.style.display = "none";
-            hiddenCard.style.display = "block";  
-        };
-        btnHide.onclick = function() {
-            visibleCard.style.display = "block";
-            hiddenCard.style.display = "none";  
-        };
-</script>
 <script src="_assets/vendor/jquery/jquery-3.4.1.min.js"></script>
 <script src="_assets/vendor/popper/popper.min.js"></script>
 <script src="_assets/vendor/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
