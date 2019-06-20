0f308fe15e1b615034278b040e31cf45c0bb3f98
l3njo <plenjo15@gmail.com>
2019-06-20 23:14:34 +0300

    Validate profile edit.
    Also edited Notification class to make it easier to work with.

--- _account/_edit.php
+++ _account/_edit.php
@@ -2,15 +2,21 @@
     // TODO Data authentication 
     if(isset($_POST["new_username"])) {
         extract($_POST);
+        $notify;
         $token = $_SESSION['token'];
-        $sql ="select username, email from users where token = '$token'";
-        $res = mysqli_query($db,$sql);
-        $row =mysqli_fetch_row($res);
-        $new_username==($new_username==="")?$row[0]:$new_username;
-        $new_email==($new_email==="")?$row[1]:$new_email;
-        $_SESSION['user'] = $new_username;
-        $sql="UPDATE users SET username = '$new_username', email = '$new_email' WHERE token = '$token'";
-        mysqli_query($db, $sql) or die(mysqli_error($db));
+        $sql = "SELECT username, email FROM users WHERE token = '$token'";
+        $row = mysqli_fetch_row(mysqli_query($db,$sql));
+        $new_username=($new_username==='')?$row[0]:$new_username;
+        $new_email=($new_email==='')?$row[1]:$new_email;
+        $sql = "SELECT email FROM users WHERE email = '$new_email'";
+        $row = mysqli_fetch_row(mysqli_query($db,$sql));
+        if (count($row) === 0 || (count($row) === 0 && in_array($new_email, $row))){
+            $_SESSION['user'] = $new_username;
+            $sql="UPDATE users SET username = '$new_username', email = '$new_email' WHERE token = '$token'";
+            mysqli_query($db, $sql) or die(mysqli_error($db));
+            $notify = new Notification(true, 'Update successful');
+        } else $notify = new Notification(false, 'That email address is already in use');
+        array_push($_SESSION['notify'], serialize($notify));
         header("location:$this_page");
         exit();
     }
@@ -19,7 +25,7 @@
         extract($_POST);
         $notify;
         if ($new_password !== $con_password) 
-            $notify = new Notification(false, "The passwords do not match.", "danger");
+            $notify = new Notification(false, 'The passwords do not match');
         else {
             $token = $_SESSION['token'];
             $old_password = md5($old_password);
@@ -28,13 +34,13 @@
             $res = mysqli_query($db, $sql);
             $password = mysqli_fetch_row($res)[0];
             if ($old_password !== $password)
-                $notify = new Notification(false, "Incorrect current password.", "danger");
+                $notify = new Notification(false, 'Incorrect current password');
             elseif ($new_password === $password)
-                $notify = new Notification(false, "Please use a different new password.", "danger");
+                $notify = new Notification(false, 'Please use a different new password');
             else {
                 $sql="UPDATE users SET password = '$new_password' WHERE token ='$token'";
                 mysqli_query($db, $sql) or die(mysqli_error($db));
-                $notify = new Notification(true, "Password changed.", "success");
+                $notify = new Notification(true, 'Password changed');
             }
         }
         
--- _includes/lib/Notification.php
+++ _includes/lib/Notification.php
@@ -4,18 +4,19 @@
         var $message;
         var $context;
 
-        function __construct($success, $message, $context) {
+        function __construct($success, $message, $context='') {
             $this->success = $success;
             $this->message = $message;
-            $this->context = $context;
+            $this->context = $context!==''?$context:
+                $success?'success':'danger';
         }
 
         function notify(){
-            $this->success = $this->success?"Success":"Failed"; ?>
-            <div class="alert alert-<?=$this->context;?> alert-dismissible fade show" role="alert">
-                <strong><?=$this->success;?>!</strong> <?=$this->message;?>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
+            $this->success = $this->success?'Success':'Failed'; ?>
+            <div class='alert alert-<?=$this->context;?> alert-dismissible fade show' role='alert'>
+                <strong><?=$this->success;?>!</strong> <?=$this->message;?>.
+                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
                 </button>
             </div>
         <?php }
